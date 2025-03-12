27661fd7c62674c4fe44d5263285fa47548ba055
Anestezia-zip <plekatybtc@gmail.com>
2025-03-12 22:00:15 +0000

    Created sidebars

--- js/script.js
+++ js/script.js
@@ -6,26 +6,25 @@ const dropdownBtn = document.querySelector('.navbar__dropdown-btn');
 const dropdown = document.querySelector('.navbar__dropdown');
 
 burger.addEventListener('click', () => {
-    // Открываем/закрываем бургер и навбар меню
-    navbarNav.classList.toggle('active');
-    navbarAuth.classList.toggle('active');
-    // Меняем бургер на крестик 
-    burger.classList.toggle('active');
+  // Открываем/закрываем бургер и навбар меню
+  navbarNav.classList.toggle('active');
+  navbarAuth.classList.toggle('active');
+  // Меняем бургер на крестик 
+  burger.classList.toggle('active');
 });
 
 // Слушаем клики по документу и закрываем navbar, если клик был не внутри
 document.addEventListener('click', (e) => {
-    if (!navbar.contains(e.target)) {
-        navbarNav.classList.remove('active');
-        navbarAuth.classList.remove('active');
-        burger.classList.remove('active');
-    }
+  if (!navbar.contains(e.target)) {
+    navbarNav.classList.remove('active');
+    navbarAuth.classList.remove('active');
+    burger.classList.remove('active');
+  }
 });
 
 // Открываем/закрываем бургер и навбар меню
 dropdownBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-    dropdown.classList.toggle('active');
+  dropdown.classList.toggle('active');
 });
 
 // Слушаем клики по документу и закрываем dropdown смены языка, если клик был не внутри
