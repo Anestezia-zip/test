11b8f96e4093d83aed02652e65ba86c341af9573
Anestezia-zip <plekatybtc@gmail.com>
2025-03-14 09:39:01 +0000

    Finished main page

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const navbarNav = document.querySelector('.navbar__nav');
 const navbarAuth = document.querySelector('.navbar__auth');
 const dropdownBtn = document.querySelector('.navbar__dropdown-btn');
 const dropdown = document.querySelector('.navbar__dropdown');
+const sidebarArrow = document.querySelector('.sidebar__arrow-mobile');
+const sidebarItems = document.querySelectorAll('.sidebar__item--live-events span');
+
 
 burger.addEventListener('click', () => {
   // Открываем/закрываем бургер и навбар меню
@@ -13,7 +16,7 @@ burger.addEventListener('click', () => {
   burger.classList.toggle('active');
 });
 
-// Слушаем клики по документу и закрываем navbar, если клик был не внутри
+// Закрываем navbar, если клик был не внутри
 document.addEventListener('click', (e) => {
   if (!navbar.contains(e.target)) {
     navbarNav.classList.remove('active');
@@ -27,9 +30,34 @@ dropdownBtn.addEventListener('click', (e) => {
   dropdown.classList.toggle('active');
 });
 
-// Слушаем клики по документу и закрываем dropdown смены языка, если клик был не внутри
+// Закрываем dropdown смены языка, если клик был не внутри
 document.addEventListener('click', (e) => {
   if (!dropdown.contains(e.target)) {
     dropdown.classList.remove('active');
+  }
+});
+
+// Открываем/закрываем faq 
+document.querySelectorAll('.faq__question').forEach(button => {
+  button.addEventListener('click', () => {
+    const item = button.parentElement;
+    item.classList.toggle('active');
+  });
+});
+
+// Открываем/закрываем sidebar items
+sidebarArrow.addEventListener('click', (e) => {
+  sidebarItems.forEach(item => {
+    item.classList.toggle('active');
+  });
+});
+
+// Удаляем класс 'active' при клике вне sidebar
+document.addEventListener('click', (e) => {
+  // Проверяем, что клик был не по элементу sidebar или по самой стрелке
+  if (!e.target.closest('.sidebar__header')) {
+    sidebarItems.forEach(item => {
+      item.classList.remove('active');
+    });
   }
 });
